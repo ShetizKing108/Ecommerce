b690da898580ca6e956cdf8eaa30a3535360d042
Abhishek_Shet <shet.is.king@gmail.com>
2022-07-23 20:46:35 +0530

    "CReated Views and superuser also registered models in admin.py"

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin): # we are extending from Admin to ModelAdmin
+    list_display = ['name', 'slug'] # these are the two things that we want to be displayed for the admin
+    prepopulated_fields = {'slug': ('name',)} # Slug will be populated with what we have typed into the name
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
