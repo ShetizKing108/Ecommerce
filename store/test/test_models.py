2fb85e89fa7c215ae1e8a130a93ebf44702be0d6
Abhishek_Shet <shet.is.king@gmail.com>
2022-07-23 22:30:22 +0530

    "Created test cases to test our Models"

--- store/test/test_models.py
+++ store/test/test_models.py
@@ -1,1 +1,36 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):   # We are setting up a DB where testing will be done
+        self.data1 = Category.objects.create(name='django', slug='django')   # We are creating objects to test them to validate our logic
+
+    def test_category_model_entry(self):
+        """
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category)) # isinstancce checks if the data(we have created in setUp above) fits our model called 'Category'
+        self.assertEqual(str(data), 'django') # The str(data) entered should match 'django'
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                                            slug='django-beginners', price='20.00', image='django')
+        self.data2 = Product.objects.create(category_id=1, title='django advanced', created_by_id=1,
+                                             slug='django-advanced', price='20.00', image='django', is_active=False)
+
+    def test_products_model_entry(self):
+        """
+        Test product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
