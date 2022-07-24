ac3d3a09c384cdf3d78df289f2d4aa081b621223
Abhishek_Shet <shet.is.king@gmail.com>
2022-07-24 22:34:29 +0530

    "CReated basket.py, summary.html and urls.py"

---
+++ basket/basket.py
@@ -0,0 +1,2 @@
+# Here we will be modelling the basket, add functionalities and call the same from the views.py
+
--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from store.models import Product
+
+from .basket import Basket
+
+
+def basket_summary(request):
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
